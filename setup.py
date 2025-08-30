0d3a06e82b31ff54bf52519d33d033f81dadd0c7
nevmenandr <nevmenandr@gmail.com>
2025-08-30 20:43:15 +0300

    minor changes, оформительское

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cgi_processor',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     description='Модуль для предобработки текстов журнала «Цифровые гуманитарные исследования»',
     long_description=open('README.md', encoding='utf-8').read(),
